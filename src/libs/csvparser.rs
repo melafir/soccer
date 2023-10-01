12181f69fb94977788b922680793e9ce84e0e53c
Melxorn <melektaus2019@gmail.com>
2023-10-01 17:01:49 +0300

    Impl choose by role

--- src/libs/csvparser.rs
+++ src/libs/csvparser.rs
@@ -1,15 +1,18 @@
 use std::fs::read_to_string;
 use std::path::Path;
 use std::str::FromStr;
-use std::io;
+use std::io::{self, Write};
 
 use crate::libs::player::Player;
+use crate::libs::roles::AttributesRole;
+
+use super::roles::Roles;
 
 fn csvparser(f:&str)->Vec<String> {
     let path = Path::new(f);
     let mut t:Vec<String> = Vec::new();
     match read_to_string(path){
-        Err(_)=>panic!("Couldn't read file"),
+        Err(e)=>panic!("{}",e),
         Ok(f)=>{
             let mut temp:Vec<&str> = f.split("\n").collect();
             for i in temp.iter_mut().skip(1){
@@ -27,19 +30,62 @@ pub fn toplayer(f:&str)->Vec<Player>{
     }
     t
 }
-pub fn choose()-> Vec<Player>{
-    print!("Choose main[1] or double[2]: ");
-    let mut inp = String::new();
-    io::stdin().read_line(&mut inp).expect("Choose main or double: ");
-    let c:u8 = inp.trim().parse().unwrap();
-    println!();
-    if c==1{
-       toplayer("forfm.csv") 
-    }else if c==2 {
-       toplayer("duble.csv") 
-    }else{
-        panic!("Choose wrong")
+pub fn choose(){
+    let mut roles_array :Vec<Roles> = Vec::new();
+    let ra = AttributesRole::make();
+
+    for (r,_) in ra.iter(){
+        roles_array.push(r.clone());
     }
-
-
+    loop{
+        roles_array.iter().enumerate().for_each(|(index,r)|{
+            if index%3==0 {
+                println!()
+            }
+            print!("{:>2} : {:>15} | ",index +1,r);
+        });
+        println!();
+        print!("Choose role to show ranking: ");
+        io::stdout().flush().unwrap();
+        let mut chrole = String::new();
+        io::stdin().read_line(&mut chrole).expect("Wrong role!");
+        let cr:usize = chrole.trim().parse().expect("Faild to parse role");
+        println!();
+        if cr==0{break;}
+        let mut f = toplayer("forfm.csv");
+        let d = toplayer("duble.csv");
+        f.extend(d);
+        let role = roles_array[cr-1];
+        let out = pr_by_roles(role, f);
+        println!("{}",role);
+        out[0..10].iter().enumerate().for_each(|(index,(name,av))|{
+            if index%3 == 0{
+                println!()
+            }
+            print!("{:<15} -> {:>5}   |   ",name,av);
+        });
+        println!();
+    }
 }
+fn choose_by_role(ar:Vec<Player>) -> Vec<(String,Roles,f32)>{
+    let mut out : Vec<(String,Roles,f32)> = Vec::new();
+    ar.iter().for_each(|i|{
+        let cal = i.calcroles();
+        cal.iter().for_each(|(r,(_,av))|{
+            out.push((i.surname.clone(),r.clone(),*av));
+        })
+    });
+    out
+}
+pub fn pr_by_roles(r:Roles,ar:Vec<Player>) -> Vec<(String,f32)>{
+    let v = choose_by_role(ar);
+    let mut out:Vec<(String, f32)> = Vec::new();
+    v.iter().for_each(|(name,role,av)|{
+        if r==*role {
+         out.push((name.clone(),*av))
+        }
+    });
+    out.sort_by(|a,b|b.1.partial_cmp(&a.1).unwrap());
+    out
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
 use soccer::libs::csvparser;
 fn main() {
-    let f = csvparser::choose(); 
-    for i in f{
-        println!("{} -> {}",i.surname,i.getattributesum());
-        i.calcroles().iter().enumerate().for_each(|(index,(r,(_,av)))|{
-            print!("{}: {:5}   |",r,av);
-            if (index+1)%3==0{
-                println!();
-            }
-        });
-        println!();
-        println!();
-    }
+    csvparser::choose();
 }
